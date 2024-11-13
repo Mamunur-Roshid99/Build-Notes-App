c9c93a52d33c729619ed5e0a4678c80092a42dbd
Mamunur-Roshid99 <roshidmamunur569@gmail.com>
2024-11-14 00:15:30 +0600

    note app

---
+++ script.js
@@ -0,0 +1,38 @@
+const notesContainer = document.querySelector(".notes-container");
+const button = document.querySelector(".btn");
+const notes = document.querySelectorAll(".input-box");
+
+function shownotes() {
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+
+shownotes();
+function updateStorage() {
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+
+button.addEventListener("click", () => {
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.className = "input-box"
+    inputBox.setAttribute("contenteditable", "true");
+    img.src = "images/delete.png";
+    notesContainer.appendChild(inputBox).appendChild(img)
+})
+
+
+notesContainer.addEventListener("click", (e) => {
+    if (e.target.tagName === "IMG") {
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if (e.target.tagName === "p") {
+        notes.forEach(nt => {
+            nt.onkeyup = function(){
+                updateStorage();
+            }
+        })
+    }
+})
+
